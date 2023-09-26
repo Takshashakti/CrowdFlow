e768e6b1e89be74ea666353348b291b7045513c8
shahank42 <crazykavlogs@gmail.com>
2023-09-27 00:42:35 +0530

    exported edge runtime for api

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -4,6 +4,8 @@ import { env } from "~/env.mjs";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
+export const runtime = "edge";
+
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
@@ -12,7 +14,7 @@ export default createNextApiHandler({
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
           console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
           );
         }
       : undefined,
