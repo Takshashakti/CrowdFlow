68cc30a70f660fab43e4212d72db2493a24c4707
jaysmito <jaysmito101@gmail.com>
2023-12-10 14:28:37 +0530

    removed unused stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,6 @@ export default function Home() {
                 </div>
                 </div>
               </SheetFooter> */}
-              <ReportIncident />
             </SheetContent>
           </Sheet>
         </div>
