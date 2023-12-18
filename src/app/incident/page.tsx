bad79a3f45723f75d01c09cd736067e9a1da1e06
shahank42 <shounakghosh42@gmail.com>
2023-12-19 01:12:37 +0530

    fuck you jaysmito

---
+++ src/app/incident/page.tsx
@@ -0,0 +1,32 @@
+import HomeSearchBar from "@/components/HomeSearchBar";
+import IncidentDetails from "@/components/IncidentDetails";
+// import MapComponent from "@/components/MapComponent";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import ReportIncident from "@/components/ReportIncidentForm";
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { cn } from "@/lib/utils";
+import { Check, Cross, CrossIcon, X } from "lucide-react";
+import dynamic from "next/dynamic";
+import Link from "next/link";
+
+const MapComponent = dynamic(() => import("@/components/MapComponent"), {
+  ssr: false,
+});
+
+export default function IncidentTemp() {
+  return (
+    <>
+    <IncidentDetails disasterID="hsdfikjgh" />
+    </>
+  );
+}
