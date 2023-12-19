167053c3bb60e46dcea53b209caa5b3fcb04a3e0
Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>
2023-12-19 17:25:07 +0530

    Notifications page  (#34)
    * Deez nuts
    
    * ]]]]
    
    * cleaned up some mess
    
    * NotificationItems
    
    ---------
    
    Co-authored-by: Neha Sinha <nehasinha123987@gmail.com>

---
+++ src/components/NotificationItems.tsx
@@ -0,0 +1,54 @@
+import { cn } from "@/lib/utils";
+import { Dialog, DialogDescription, DialogTitle, DialogTrigger } from "@radix-ui/react-dialog";
+import { ReactNode } from "react";
+import { DialogContent, DialogHeader } from '@/components/ui/dialog';
+import { Button } from "./ui/button";
+
+
+interface ListIemProps {
+  children: ReactNode;
+  className?: String;
+}
+
+const NotificationItems: React.FC<ListIemProps> = ({children, className}) => {
+    return <div className={cn(`
+    flex
+    justify-start
+    w-full
+    text-center
+    items-center
+    py-2 px-2
+    bg-slate-100 
+    backdrop-blur-sm 
+    my-2
+
+    `, className)
+    }>
+      
+        <div className="font-medium text-left truncate w-[65%]">
+          {children}
+        </div>
+
+        <Dialog>
+              <DialogTrigger className="bg-black w-[35%] px-2 py-2 text-center items-center rounded-md text-white">
+                View Details
+              </DialogTrigger> 
+              <DialogContent className="px-6">
+                <DialogHeader>
+                  <DialogTitle className="text-2xl font-bold text-center">
+                    Report Details
+                  </DialogTitle>
+                  <br></br>
+                  <DialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    your account and remove your data from our servers.
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+  
+      
+      </div>;
+  };
+  
+  export default NotificationItems;
